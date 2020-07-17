638638daddc2eee2edabc73c8315706d63300bab
Amer-bit <ny.1995@outlook.com>
2020-07-17 11:43:47 +0300

    almostDone

--- src/context/auth/login.js
+++ src/context/auth/login.js
@@ -29,7 +29,7 @@ class Login extends React.Component {
         return(
             <>
          <Show condition = {this.context.loggedIn}>
-             <button onClick = {() => this.context.logout}> Logout </button>
+             <button onClick = {this.context.logout}> Logout </button>
          </Show>
          <Show condition={!this.context.loggedIn}>
              <form onSubmit = {this.handleSubmit}>
